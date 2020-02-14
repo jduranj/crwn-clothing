3eef7529214f214efde488207f052d06c04ac726
Jesús Durán <47229820+jduranj@users.noreply.github.com>
2020-02-14 16:00:45 -0600

    fix typo

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,7 +36,7 @@ const CheckoutPage = ({ cartItems, total }) => (
             <span>TOTAL: ${total}</span>
         </div>
         <div className='test-warning'>
-            *Please use the following test credit card for paymnets*
+            *Please use the following test credit card for payments*
             <br/>
             4242 4242 4242 4242 - Exp 01/20 - CVV: 123
         </div>
